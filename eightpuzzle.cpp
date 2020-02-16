8584329acb87d8799adcef4dfe2a1ea53c5df707
Cristian Reyes <cris178@outlook.com>
2020-02-15 23:14:04 -0800

    state representation was wrong

--- eightpuzzle.cpp
+++ eightpuzzle.cpp
@@ -1,75 +1,111 @@
 #include <iostream>
 #include <string>
+#include <queue>
+#include <cstring>       //for memcpy
+#include <bits/stdc++.h> //
 using namespace std;
 
 /****************************************************************/
 
+/*
+cost function
+c(x) = g(x) + f(x)
+g(x) = cost of reaching current node from root #length of path from root to x
+f(x) = cost of reaching goal from current node # of non blank tiles not in their goal position
+*/
+
+int checkCost(int init[3][3], int goal[3][3])
+{
+    int count = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            //relational operators have higher precedense
+            if (init[i][j] && (init[i][j] != goal[i][j])) //Check this later, WORKS
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
 class Node
 {
 public:
-    Node();                      //Default constructor
-    Node(int val, int r, int c); //Constructor
-    const int getNumber() const; //Display number is just the visual title
-    const int getRow() const;
-    const int getColumn() const;
-    void setRow(int x);
-    void setColumn(int x);
+    Node();                                      //Default constructor
+    Node(int[3][3], int r, int c, int pathcost); //Constructor
+    int checkCost(int arr[3][3], int goal[3][3]);
     Node &operator=(const Node &rhs)
     {
-        if (this != &rhs)
-        {
-            this->number = rhs.getNumber();
-            this->row = rhs.getRow();
-            this->column = rhs.getColumn();
-        }
-        return *this;
     }
+    int row;    //Row where 0 is
+    int column; //Column where 0 is
+    int matrix[3][3];
+    int depth;
+    int cost;
+    Node *parent; //pointer to parent
 
 private:
-    int number; // title of the node
-    int row;
-    int column;
-    Node *parent; //pointer to parent
 };
 
 Node::Node()
 {
-    number = 0;
 }
 
-Node::Node(int val, int r, int c)
+Node::Node(int arr[3][3], int r, int c, int pathcost)
 {
-    //cout << "Node value is : " << val << "\n";
-    number = val;
+    //Getting the State and setting it as our Node structure.
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            matrix[i][j] = arr[i][j];
+        }
+    }
     row = r;
     column = c;
-}
-
-const int Node::getNumber() const
-{
-    return number;
-}
-
-const int Node::getRow() const
-{
-    return row;
-}
-
-const int Node::getColumn() const
-{
-    return column;
-}
-
-void Node::setRow(int x)
-{
-    row = x;
-}
-void Node::setColumn(int x)
-{
-    column = x;
+    cost = pathcost;
 }
 
 /******************************************************************/
+
+/********************************************************************/
+//This is how we will create new nodes.
+//Each time a new node is created this is like a constructor
+Node *newNode(int arr[3][3], int x, int y, int newX, int newY, int level, Node *parent)
+{
+    Node *node = new Node;
+
+    // set pointer for path to root
+    node->parent = parent;
+
+    // copy data from parent node to current node
+    //http://www.cplusplus.com/reference/cstring/memcpy/
+    memcpy(node->matrix, arr, sizeof node->matrix);
+
+    //If we pass in a higher value in newx and newy this will shift
+    //the 0 producing a new state
+    // move tile by 1 position
+    swap(node->matrix[x][y], node->matrix[newX][newY]);
+
+    // set number of misplaced tiles
+    //Max cost (infiniti) until proper value is given
+    //https://stackoverflow.com/questions/3233054/error-int32-max-was-not-declared-in-this-scope
+    node->cost = INT_MAX;
+
+    // The depth of our soulution so far.
+    node->depth = level;
+
+    // If we pass in a newx or y coordinate we will update those values
+    node->row = newX;
+    node->column = newY;
+
+    return node;
+}
+
+/***********************************************************************************/
 
 /*******************************************************************/
 
@@ -77,10 +113,6 @@ class Problem
 {
 public:
     Problem();
-    Node *initialState;
-    Node *goalState;
-    void setInitialState(Node temp);                  //Insert node to be initialstate;
-    void setGoalState(Node temp);                     //Insert node to be initialstate;
     void checkOP(Node temp);                          //This function checks which operation is available on provided node
     void moveLeft(Node arr[3][3], int row, int col);  //Operator move node to the left
     void moveRight(Node arr[3][3], int row, int col); //Operator move node to the right
@@ -93,123 +125,48 @@ Problem::Problem()
     cout << "A problem has been constructed.\n";
 }
 
-void Problem::setInitialState(Node temp)
+//Each node can have max four children left,right,top,or bot as possible operators
+int checkOP(int x, int y)
 {
-    initialState = &temp;
-}
-
-void Problem::setGoalState(Node temp)
-{
-    goalState = &temp;
-}
-
-void Problem::checkOP(Node temp)
-{
-    //if
-
-    //elseif
-
-    //elseif
-
-    //elseif
-}
-
-void Problem::moveUp(Node arr[3][3], int row, int col)
-{
-
-    if (row <= 0) //Can't move if at index 0 would be out
+    //This function see's what child states are viable
+    //from the current state. If possible returns true else false;
+    if (x >= 0 && x < 3 && y >= 0 && y < 3)
     {
-        cout << "Can't move that tile Up.\n";
-        return;
-    }
-    else if (arr[row - 1][col].getNumber() == 0)
-    {
-        Node shifting = arr[row - 1][col]; //Save 0 to temp
-        arr[row - 1][col] = arr[row][col]; //assign appropriate to 0 spot
-        arr[row][col] = shifting;          //temp in old spot
-        cout << "Moved " << arr[row - 1][col].getNumber() << " up.\n";
+        return 1;
     }
     else
     {
-        cout << "No blank space available to move towards.\n";
+        return 0;
     }
 }
 
-void Problem::moveDown(Node arr[3][3], int row, int col)
-{
-
-    if (row >= 2)
-    {
-        cout << "Can't move that tile down.\n";
-        return;
-    }
-    else if (arr[row + 1][col].getNumber() == 0)
-    {
-        Node shifting = arr[row + 1][col];
-        arr[row + 1][col] = arr[row][col];
-        arr[row][col] = shifting;
-        cout << "Moved " << arr[row + 1][col].getNumber() << " down.\n";
-    }
-    else
-    {
-        cout << "No blank space available to move towards.\n";
-    }
-}
-
-void Problem::moveLeft(Node arr[3][3], int row, int col)
-{
-
-    if (col <= 0)
-    {
-        cout << "Can't move that tile Left.\n";
-        return;
-    }
-    else if (arr[row][col - 1].getNumber() == 0)
-    {
-        Node shifting = arr[row][col - 1];
-        arr[row][col - 1] = arr[row][col];
-        arr[row][col] = shifting;
-        cout << "Moved " << arr[row][col - 1].getNumber() << " Left.\n";
-    }
-    else
-    {
-        cout << "No blank space available to move towards.\n";
-    }
-}
-
-void Problem::moveRight(Node arr[3][3], int row, int col)
-{
-
-    if (col >= 2)
-    {
-        cout << "Can't move that tile Right.\n";
-        return;
-    }
-    else if (arr[row][col + 1].getNumber() == 0)
-    {
-        Node shifting = arr[row][col + 1];
-        arr[row][col + 1] = arr[row][col];
-        arr[row][col] = shifting;
-        cout << "Moved " << arr[row][col + 1].getNumber() << " Right.\n";
-    }
-    else
-    {
-        cout << "No blank space available to move towards.\n";
-    }
-}
-
-void displayState(Node arr[3][3], int size) //Display State
+void displayState(int arr[3][3]) //Display State
 {
     cout << "Printing Puzzle ---------------\n";
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < size; j++)
+        for (int j = 0; j < 3; j++)
         {
-            cout << arr[i][j].getNumber() << " ";
+            cout << arr[i][j] << " ";
         }
         cout << "\n";
     }
+}
+
+// print path from root node to destination node
+//https://www.dummies.com/programming/cpp/how-to-pass-pointers-to-functions-in-c/
+void printPath(Node *root)
+{
+    //Recursively call self until your reach the root node.
+    if (root == NULL)
+    {
+        return;
+    }
+    printPath(root->parent);
+    displayState(root->matrix);
+
+    printf("\n");
 }
 
 /**************************************************************************/
@@ -219,7 +176,47 @@ void displayState(Node arr[3][3], int size) //Display State
 //Initial State:
 //Goal State:
 //Operators:
+//First check for trick problem where inital state is goal state.
+//If it's not, need to expand the current state, which means apply each legal action to the current state.
+//Which will generate new states
+
+//root node
+// 1 3
+// 2 0
+//Can generate
+
+//branch 1
+//1 0
+//2 3
+
+//branch 2
+//1 3
+//0 2
+
+//Here we added two branches to the parent node
+//We then consider which to continue branching out from
+//We now check to see if it is a goal state, if not expand it
+
+//branch 1b
+//0 1
+//2 3
+
+//branch 1c
+//1 3
+//2 0
+
+//We can expand these branch 2 sub nodes or expand branch 2
+//branches 1b, 1c, and 2 are in the frontier, we decide which to expand in the frontier
+//This process continues until a solution is found or no more states to expand.
+
 // Apply Goal Test when: NOT when when generating a node because could find better nodes by expanding siblings in frontier
+
+//The first is that the goal test is applied to a node when
+//it is selected for expansion (as in the generic graph-search algorithm shown in Figure 3.7)
+//rather than when it is first generated.
+
+//The second difference is that a test is added in case a better
+//path is found to a node currently on the frontier.
 
 /********************************************************************************/
 
@@ -234,10 +231,18 @@ int main()
         cin >> option;
     }
 
-    Node puzzle[3][3]; //Ourpuzzle 2D array
+    int puzzle[3][3];                              //Ourpuzzle 2D array
+    int explored[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //If node contains goal state, add node.number to explaored array
+                                                   //if frontier node matches explored node, discard it.
+
+    priority_queue<Node> frontier; //Our frontier is a queue http://www.cplusplus.com/reference/queue/queue/
+    int goalState[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 0, 8}};
 
     if (option == 1)
-    {
+    { /*
         cout << "Default option selected.\n";
         int eight = 1;
 
@@ -255,28 +260,41 @@ int main()
                 {
                     Node temp(eight, i, j);
                     puzzle[i][j] = temp;
-                    /*cout << "--Entered in Array "
-                         << "[" << i << "]"
-                         << ","
-                         << "[" << j << "]"
-                         << " the value " << puzzle[i][j].displayNumber() << "\n";*/
                     eight++;
                 }
             }
-        }
+        }*/
     }
     else
     {
-        while (count < 0 || count > 8)
+
+        int val = 0;
+        int x, y = 0;
+        for (int i = 0; i < 3; i++)
         {
-            cout << "Welcome to Cristian's creye025 8 puzzle solver\n";
-            cout << "Type 1 for the default puzzle or 2 to enter your own puzzle\n";
-            cout << "Eight Puzzle Solver\n";
+            for (int j = 0; j < 3; j++)
+            {
+                cout << "Enter the number 0-8 you want in position [" << i << "," << j << "], with 0 being the blank space" << endl;
+                cin >> val;
+                puzzle[i][j] = val;
+                if (val == 0)
+                {
+                    x = i;
+                    y = j;
+                }
+                val = 0;
+            }
         }
+        int cost = checkCost(puzzle, goalState);
+
+        //This is the initial root node constructed
+        //puzzle is initial state, x,y is 0 position, second pair is where to move new nodes
+        Node *root = newNode(puzzle, x, y, x, y, 0, NULL);
+        cout << checkCost(puzzle, goalState) << " This is the cost\n";
     }
 
-    displayState(puzzle, 3); //Need to send the arrays address https://stackoverflow.com/questions/8767166/passing-a-2d-array-to-a-c-function
-
+    displayState(puzzle); //Need to send the arrays address https://stackoverflow.com/questions/8767166/passing-a-2d-array-to-a-c-function
+    /*
     Problem a1;
     a1.moveDown(puzzle, 1, 2);
     displayState(puzzle, 3);
@@ -294,7 +312,7 @@ int main()
     displayState(puzzle, 3);
     a1.moveUp(puzzle, 1, 0);
     displayState(puzzle, 3);
-
+    */
     /*
 
     //Reset for next options
